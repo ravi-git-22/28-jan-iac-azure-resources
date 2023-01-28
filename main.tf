dda9f10cb511fa2e16046e5e23732d3e40c5084c
ravi-git-22 <ravikiran.thames@outlook.com>
2023-01-28 16:39:50 +0000

    commit 13terraform files created and secrets configured in github desktop

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-resource "random_string" "random" 
-{
-  length           = 16
-  special          = true
-  override_special = "/@£$"
-}
 
-resource "azurerm_resource_group" "terraform_resource_group" 
-{
-  name     = random_string.random
+resource "azurerm_resource_group" "terraform_resource_group" {
+  name     = randomstringalibaba
   location = var.location
 }
 
-resource "azurerm_storage_account" "terraform_storage_account" 
-{
+resource "azurerm_storage_account" "terraform_storage_account" {
   name                = "storageterraform0914"
   resource_group_name = azurerm_resource_group.terraform_resource_group.name
   location                 = azurerm_resource_group.terraform_resource_group.location
