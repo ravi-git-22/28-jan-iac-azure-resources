f8ca1f572eada0b0258e654176eff5451f6a7297
ravi-git-22 <ravikiran.thames@outlook.com>
2023-01-28 16:47:46 +0000

    commit 15 terraform files created and secrets configured in github desktop

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "terraform_resource_group" {
 }
 
 resource "azurerm_storage_account" "terraform_storage_account" {
-  name                = "storageterraform0914"
+  name                = "storagetxxerraform0914"
   resource_group_name = azurerm_resource_group.terraform_resource_group.name
   location                 = azurerm_resource_group.terraform_resource_group.location
   account_tier             = "Standard"
