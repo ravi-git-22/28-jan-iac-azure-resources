46e2cd6e5dbc1ecfcf2cd81e6f7a03df2e6ef459
ravi-git-22 <ravikiran.thames@outlook.com>
2023-01-28 17:00:18 +0000

    commit 17 commenst.txt changed terraform files created and secrets configured in github desktop

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 
 resource "azurerm_resource_group" "terraform_resource_group" {
-  name     = "randomstringalibaba444"
+  name     = "randomstringalibabax444"
   location = var.location
 }
 
 resource "azurerm_storage_account" "terraform_storage_account" {
-  name                = "storagetxxerraform0914"
+  name                = "storagetxxerxraform0914"
   resource_group_name = azurerm_resource_group.terraform_resource_group.name
   location                 = azurerm_resource_group.terraform_resource_group.location
   account_tier             = "Standard"
